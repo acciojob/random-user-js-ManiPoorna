df00103f4a23c3ba9d735d66c6fd988a67cb1d75
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-19 11:53:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+const image = document.getElementById('image');
+const name = document.getElementById('name');
+const age = document.getElementById('age');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const info = document.getElementById('info');
+document.getElementById("getuser").addEventListener("click",getuser)
+
+let user = getuser();
+async function getuser() {
+    const response = await fetch("https://randomuser.me/api/")
+    const data = await response.json();
+    user = (data.results[0])
+    console.log(user)  
+    info.innerText = ''
+    image.src = user.picture.large
+    name.innerText = user.name.first +" "+ user.name.last
+
+    return user
+}
+console.log(user)
+age.addEventListener('click',()=>{
+    info.innerText = user.dob.age
+})
+email.addEventListener('click',()=>{
+    info.innerText = user.email
+})
+phone.addEventListener('click',()=>{
+    info.innerText = user.phone
+})
